88f5a453f357d27e0424f6a98d0198e4b28624e9
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-04-23 09:38:16 +0200

    clean up example case

--- clustering.py
+++ clustering.py
@@ -34,55 +34,16 @@ def dbscan_clustering(data, nfolds):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--missingness', '-m', dest='missingness', type=str, default='real')
-    parser.add_argument('--frac_missing', '-p', dest='frac_missing', type=float, default=None)
-    parser.add_argument('--debug', dest='idebugspace', action='store_true')
-    parser.add_argument('--no-debug', dest='idebugspace', action='store_false')
-    parser.set_defaults(idebugspace=True)
-    args = parser.parse_args()
-
-    # data settings
-    missingness = args.missingness
-    frac_missing = args.frac_missing
-    idebugspace = args.idebugspace
-    icrossval = False
-    if icrossval:
-        from namelist import crossvalpath
-        largefilepath = crossvalpath
-    else:
-        from namelist import largefilepath
-
     # method settings
-    bagging_method = 'kmeans'
-    if idebugspace:
-        epochs = np.arange(20,60,1)
-    else:
-        epochs = np.arange(50,150,1)
+    epochs = np.arange(50,150,1)
     random.seed(0)
     epochs = random.choices(epochs, k=3) 
 
     # read feature table
-    logging.info(f'read feature table...')
-    # ATTENTION THESE ARE NOT YET THE SAME
-    # differences larger than float point uncertainties in tp and swvl1, smaller in skt, none at all in tws
-    # for all timesteps (seasonality pattern) and especially places on earth with high missingness
-    # idea: something wrong / different in interpolation function? since now temporal and variable mean are calculated before any gapfilling happens
-    # import IPython; IPython.embed()
-    # OLD
-    #init_impute = 'smart'
-    #data = xr.open_dataset(largefilepath + f'features_init_{missingness}_{frac_missing}_{init_impute}_idebug_{idebugspace}.nc').load()
-    #lostmask = xr.open_dataset(largefilepath + f'lostmask_init_{missingness}_{frac_missing}_{init_impute}_idebug_{idebugspace}.nc').load()
-    #data = data['__xarray_dataarray_variable__'].T
-    #logging.info(f'preproc shizzl...')
-    #lostmask = lostmask['tp'].T
-    #lostmask = lostmask.stack(datapoints=('time','landpoints')).T
-    # NEW
-    data = xr.open_dataset(largefilepath + f'features_init_{missingness}_{frac_missing}_idebug_{idebugspace}.nc')
-    lostmask = xr.open_dataset(largefilepath + f'lostmask_init_{missingness}_{frac_missing}_idebug_{idebugspace}.nc')
+    data = xr.open_dataset('/path/to/gappy/dataset')
+    mask = xr.open_dataset('/path/to/mask/dataset')
     data = data['data'] # netcdf can only save datasets, we use only dataarrays because of more convenient indexing options
-    lostmask = lostmask['data']
-    #import IPython; IPython.embed()
+    mask = mask['data']
     
     # create clusters and save
     for e in epochs:
@@ -90,8 +51,8 @@ if __name__ == '__main__':
         labels = kmeans_clustering(data, nfolds=e)
         for f in range(e):
             logging.info(f'start fold {f}...')
-            batch = data[labels == f,:]
-            batchlost = lostmask[labels == f,:]
+            databatch = data[labels == f,:]
+            maskbatch = mask[labels == f,:]
 
-            batch.to_dataset(name='data').to_netcdf(largefilepath + f'features_init_label_e{e}f{f}_{bagging_method}_{missingness}_{frac_missing}_idebug_{idebugspace}.nc')
-            batchlost.to_dataset(name='data').to_netcdf(largefilepath + f'lostmask_init_label_e{e}f{f}_{bagging_method}_{missingness}_{frac_missing}_idebug_{idebugspace}.nc', encoding={'data':{'dtype':'bool'}}) # needs explicit bool otherwise lostmask is saved as int (0,1) and numpy selects all datapoints as missing in imputethis since 0 and 1 are treated as true and no false are found
+            # databatch.to_netcdf ...
+            # maskbatch.to_netcdf ...
