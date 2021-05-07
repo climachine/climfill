0afaf690a94cd449850143d64ef7c9de52545d0d
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-05-07 15:17:33 +0200

    add all imports

--- climfill/__init__.py
+++ climfill/__init__.py
@@ -1,5 +1,10 @@
 # flake8: noqa
 
+from . import core
+from .core import interpolation
+from .core import feature_engineering
 from .core import clustering
+from .core import regression_learning
+from .core import postproc
 
 # add the other required top-level functions from the other py files
