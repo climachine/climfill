353b845dcf8ca2c97e320aac2ab853cc5e48acca
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-01-13 15:33:39 +0100

    replace spatiotemporal interpolation with thin-plate-spline + kriging

--- climfill/interpolation.py
+++ climfill/interpolation.py
@@ -14,26 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 This file gives you the function for filling any missing points
-with the spatiotemporal mean of its surrounding points
-(interpolation step) and a convenience function for removing points
-outside the area of interest (for example, ocean) from the dataset.
+with a spatial interpolation (interpolation step) 
 """
 
 import numpy as np
 import xarray as xr
-from scipy import LowLevelCallable
-from scipy.ndimage.filters import generic_filter
-
-from .numba_nanmean import nbnanmean
-
-# fast spatiotemporal mean with cython
-mean_fct = LowLevelCallable(nbnanmean.ctypes)
-
-
-def log_fracmis(data, logtext=""):
-    frac_mis = (np.isnan(data).sum() / data.size).values
-    print(f"fraction missing {logtext}: {frac_mis}")
-
+from scipy.interpolate import RBFInterpolator
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process import GaussianProcessRegressor
 
 def gapfill_interpolation(data, n=5):
     """
@@ -81,32 +69,122 @@ def gapfill_interpolation(data, n=5):
 
     return data
 
-
-def remove_ocean_points(data, landmask):
+def gapfill_thin_plate_spline(data_monthly, landmask, rbf_kwargs):
     """
-    Compress data cube by removing all ocean points from the data. The data
-    cube that has the dimensions latitude and longitude will be compressed
-    such that the returned data cube has replaced the dimensions latitude and
-    longitude by the dimension landpoints. Can also be used to remove all
-    points irrelevant for further analysis (region zoom in, remove glaciated
-    regions, only look at one country, only look at ocean,...)
+    Impute (i.e. Gapfill) data by applying thin-plate-spline interpolation
+    on each variable and each timestep independently.
 
     Parameters
     ----------
-    data: xarray dataarray, coordinates including latitude and longitude
+    data_monthly: xarray dataarray, with coordinates time, lat, lon
+        and variable
 
-    landmask: boolean xarray dataarray with only the coordinates latitude and
-        longitude, where grid points on land are True and grid points in the
-        ocean (or regions that are not relevant for research) are False
+    landmask: xarray dataarray, boolean, where land is True and ocean is
+        False
+
+    rbf_kwargs: dict, with the keys being the names of all the variables
+        in the data and the values dictionaries specifiying the parameter
+        settings for the scipy.interpolate.RBFInterpolator
 
     Returns
     ----------
-    imputed_data: data of the same shape as input data, where all values that
-        were not missing are still the same and all values that were originally
-        missing are imputed via spatiotemporal mean
+    imputed_data: xarray dataarray, data of the same shape as input data,
+        where all values that were not missing are still the same and all
+        values that were originally missing are imputed 
     """
-    landlat, landlon = np.where(landmask)
-    return data.isel(
-        longitude=xr.DataArray(landlon, dims="landpoints"),
-        latitude=xr.DataArray(landlat, dims="landpoints"),
-    )
+
+    varnames = data_monthly.coords["variable"].values
+    xx, yy = np.meshgrid(data_monthly.lon, data_monthly.lat)
+
+    for month in data_monthly.month:
+        for varname in varnames:
+
+            # select month and variable
+            tmp = data_monthly.sel(month=month, variable=varname)
+            missing = landmask & np.isnan(tmp)
+            notna = tmp.notnull()
+
+            # select only missing points
+            xy_obs = np.c_[xx[notna.values], yy[notna.values]] 
+            xy_mis = np.c_[xx[missing.values], yy[missing.values]] 
+            y_obs = tmp.values[notna]
+
+            # interpolate missing values
+            interpolator = RBFInterpolator(xy_obs, y_obs, **rbf_kwargs[varname])
+            try:
+                res = interpolator(xy_mis)
+            except Exception as e:
+                print(f'{month.item()}, {varname}, {e} in RBFInterpolator occurred, gaps not filled')
+                res = np.full_like(xy_mis[:,0], np.nan)
+
+            # save result
+            data_monthly.loc[varname, month,:,:].values[missing] = res
+    
+    return data_monthly
+
+def gapfill_kriging(data_anom, landmask, kriging_kwargs):
+    """
+    Impute (i.e. Gapfill) data by applying spatial kriging
+    on each variable and each timestep independently.
+
+    Parameters
+    ----------
+    data_monthly: xarray dataarray, with coordinates time, lat, lon
+        and variable
+
+    landmask: xarray dataarray, boolean, where land is True and ocean is
+        False
+
+    kriging_kwrags: dict, with the keys being the names of all the variables
+        in the data and the values also being dictionaries specifiying the 
+        parameter settings for the sklearn.gaussian_process.GaussianProcessRegressor
+        as well as two keys specifying the number of repeats ('repeats')
+        and the number of points considered ('npoints') for each fitting.
+
+    Returns
+    ----------
+    imputed_data: xarray dataarray, data of the same shape as input data,
+        where all values that were not missing are still the same and all
+        values that were originally missing are imputed
+    """
+
+    varnames = data_anom.coords["variable"].values
+    xx, yy = np.meshgrid(data_anom.lon, data_anom.lat)
+
+    for day in data_anom.time[:2]: # DEBUG not to take too long
+        for varname in varnames:
+
+            # select day and variable
+            tmp = data_anom.sel(time=day, variable=varname)
+            missing = landmask & np.isnan(tmp)
+            y = tmp.values[~np.isnan(tmp.values)]
+
+            # select only missing values
+            xy_test = np.c_[xx[missing.values], yy[missing.values]]
+            xy_train = np.c_[xx[~np.isnan(tmp.values)], yy[~np.isnan(tmp.values)], y]
+            if xy_test.size == 0:
+                print(f'{day.values} {varname} SKIPPED no missing points')
+                continue
+        
+            # gapfill missing values
+            k, n = kriging_kwargs[varname]['repeats'], kriging_kwargs[varname]['npoints']
+            constant_value = kriging_kwargs[varname]['constant_value']
+            length_scale = kriging_kwargs[varname]['length_scale']
+            res = np.full((k,xy_test.shape[0]), np.nan)
+
+            for i in range(k):
+                bounds = (1e-6,1e6)
+                np.random.shuffle(xy_train)
+                kernel = C(constant_value) * RBF(length_scale)
+                gp = GaussianProcessRegressor(kernel, copy_X_train=False)
+                try:
+                    gp.fit(xy_train[:n,:2],xy_train[:n,-1])
+                except ValueError as e:
+                    print(f'{e} {varname} SKIPPED')
+                Z = gp.predict(xy_test)
+                res[i,:] = Z
+            
+            # save result 
+            data_anom.loc[varname, day, :,:].values[missing.values] = res.mean(axis=0)
+
+    return data_anom
